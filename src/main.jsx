4462fa1a3b4c1847613cc78e6fc452c3ab13ee16
CoralArteaga <72654096+CoralArteaga@users.noreply.github.com>
2025-12-21 20:58:24 +0100

    main routes changed

--- src/main.jsx
+++ src/main.jsx
@@ -10,28 +10,39 @@ import FoldersPage from './folders.jsx'
 import SettingsPage from './settings.jsx'
 
 import { AuthContextProvider } from './context/UserAuth.jsx';
-import { createBrowserRouter, HashRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, HashRouterHash, Router, Routes, Route  RouterProvider } from 'react-router-dom'
 import { NotesProvider } from './context/NotesContext';
 
 //Este desarrollo se ha realizado con el apoyo de la documentación de firebase y
 // el siguiente vídeo tutorial: https://youtu.be/cZAnibwI9u8?si=SVHk00iUikfFfZHk
 
+//Se recogen las páginas en el BrowserRouter para navegar
+const router = createBrowserRouter([
+  {path:"/",element:<App/>},
+  {path:"/home",element:<Home/>},
+  {path: "/timer", element:<TimerPage/>},
+  {path: "/folders", element:<FoldersPage/>},
+  {path: "/notes", element:<NotesPage/>},
+  {path: "/settings", element:<SettingsPage/>}
 
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthContextProvider>
       <NotesProvider>
+        <RouterProvider router={router} />
         <HashRouter>
           <Routes>
             <Route path="/" element={<App />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/timer" element={<TimerPage />} />
-            <Route path="/folders" element={<FoldersPage />} />
             <Route path="/notes" element={<NotesPage />} />
-            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/timer" element={<TimerPage />} />
+            <Route path="/folders" element={<FolderPage />} />
+            <Route path="/settings" element={<FolderPage />} />
+            {/* <Route path="/Timer" element={<Timer />} /> */}
           </Routes>
-        </HashRouter>
+      </HashRouter>
       </NotesProvider>
     </AuthContextProvider>
   </StrictMode>,
